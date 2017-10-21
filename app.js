a1e2c48ac6973f78589cac4839f418a3bb1a389d
Vishakha <vishakha@coursera.com>
2017-10-21 07:00:34 -0700

    adding new files

---
+++ app.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+    angular.module("NameCalculator", [])
+    .controller('NameCalculatorCtrl', function ($scope) {
+        $scope.name = '';
+        $scope.totalVal = 0;
+
+        $scope.displayNumeric = function(){
+            var totalNameValue = calculateNumericForString($scope.name);
+            $scope.totalVal = totalNameValue;
+        }
+
+        function calculateNumericForString(string) {
+            var totalStringVal = 0;
+            for(var i=0; i< string.length; i++){
+                totalStringVal += string.charCodeAt(i);
+            }
+            return totalStringVal;
+        }
+    });
+})();
